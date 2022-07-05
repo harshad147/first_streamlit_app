8dae511fd8035cbd4f7083eae3990fd7c98209de
harshad singh <108717965+harshad147@users.noreply.github.com>
2022-07-05 13:45:19 +0530

    Added new item and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('1) Omega 3 & Blueberry Oatmeal')
+streamlit.text('2) Kale, Spinach & Rocket Smoothie')
+streamlit.text('3) Hard-Boiled Free-Range Egg')
+streamlit.text('4) Avocado Toast')
+
+streamlit.header('Build Your own Fruit Smoothie')
