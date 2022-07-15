ac7cb2a5efe781473bb3054049ce095eeb74af09
harshad singh <108717965+harshad147@users.noreply.github.com>
2022-07-15 23:22:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -8,3 +9,4 @@ streamlit.text('3) Hard-Boiled Free-Range Egg')
 streamlit.text('4) Avocado Toast')
 
 streamlit.header('Build Your own Fruit Smoothie')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
