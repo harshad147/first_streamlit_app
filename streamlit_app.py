7f3fa3717c277453bc8d72bcfa649ab406346517
harshad singh <108717965+harshad147@users.noreply.github.com>
2022-07-15 23:23:11 +0530

    added pandas function

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,4 @@ streamlit.text('4) Avocado Toast')
 
 streamlit.header('Build Your own Fruit Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
